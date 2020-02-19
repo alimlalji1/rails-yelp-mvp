89b98de6d818ecd95f7885fb7bbc061030bce3e4
Alim Lalji <alimlalji1@gmail.com>
2020-02-19 13:25:33 +0000

    02 Yelp MVP - added seed file & run db:seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  cuisine = %w[chinese italian japanese french belgian]
+  CUISINE = %w[chinese italian japanese french belgian]
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: cuisine }
+  validates :category, inclusion: { in: CUISINE }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+20.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CUISINE.sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
